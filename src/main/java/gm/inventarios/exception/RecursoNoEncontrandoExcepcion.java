eda26f73801c53d84f5a6be5b7a4c4bc333634de
josheme15 <joshicefrog@gmail.com>
2025-03-27 23:50:51 -0500

    Initial commit

---
+++ src/main/java/gm/inventarios/exception/RecursoNoEncontrandoExcepcion.java
@@ -0,0 +1,11 @@
+package gm.inventarios.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecursoNoEncontrandoExcepcion extends RuntimeException{
+    public RecursoNoEncontrandoExcepcion(String mensaje){
+        super(mensaje);
+    }
+}
